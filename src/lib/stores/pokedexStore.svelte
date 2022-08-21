e0d1ada2595add78b0ef7c40f0556c7669ec5f45
Verfindel <are.litleskare@nurofy.no>
2022-08-21 16:07:17 +0200

    Now using pokenode-ts

--- src/lib/stores/pokedexStore.svelte
+++ src/lib/stores/pokedexStore.svelte
@@ -1,46 +1,22 @@
-<script lang="ts">
-    import type { Pokeman } from "../jsonConverters/pokemanConverter";
-    import { onMount } from "svelte";
-    import { Query } from "$lib/api/query";
+<script lang="ts" context="module">
+    import { PokemonClient } from "pokenode-ts";
     import { writable } from "svelte/store";
-    import { get } from "../api/api"
-    import { Convert } from "../jsonConverters/pokemanConverter"
+
+    const client = new PokemonClient();
+
+    export const pokeman1 = writable<Pokemon>();
+    export const pokeman2 = writable<Pokemon>();
+</script>
+
+<script lang="ts">
+    import type { Pokemon } from "pokenode-ts";
     import { randomNumber } from "$lib/generators/randomNumber";
 
-    export const pokeman1 = writable<Pokeman>();
-    export const pokeman2 = writable<Pokeman>();
-
-    onMount(createPokemon);
-
-    let query = new Query();
-    query.base = "https://pokeapi.co/api/v2/pokemon";
-    query.body = "";
-
     export async function createPokemon() {
-        query.path = randomNumber(1, 151, -1).toString();
-        const res = await get(query);
-        query.path = randomNumber(1, 151, Number.parseInt(query.path)).toString();
-        const res2 = await get(query);
-        
-        const parsedData = res.results.map((result: any) => {
-            try {
-                let action = Convert.toPokeman(JSON.stringify(result));
-                return action;
-            } catch (error) {
-                console.log('Pokeman error: ', error);
-            }
-        }, []);
+        let pokemon1 = await client.getPokemonById(randomNumber(1, 151, -1));
+        let pokemon2 = await client.getPokemonById(randomNumber(1, 151, pokemon1.id));
 
-        const parsedData2 = res2.results.map((result: any) => {
-            try {
-                let action = Convert.toPokeman(JSON.stringify(result));
-                return action;
-            } catch (error) {
-                console.log('Pokeman error', error);
-            }
-        }, []);
-
-        pokeman1.set(parsedData);
-        pokeman2.set(parsedData2);
+        pokeman1.set(pokemon1);
+        pokeman2.set(pokemon2);
     }
 </script>
