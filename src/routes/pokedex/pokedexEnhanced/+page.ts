cf3144da98441f30761f5faf8416cce8846d09a3
Verfindel <are.litleskare@nurofy.no>
2022-10-02 14:01:23 +0200

    Fix: Not using data in +page.svelte, so load does not return anything.

--- src/routes/pokedex/pokedexEnhanced/+page.ts
+++ src/routes/pokedex/pokedexEnhanced/+page.ts
@@ -1,11 +1,10 @@
 import { error } from '@sveltejs/kit';
-import { GameClient, type NamedAPIResource } from "pokenode-ts";
+import { GameClient } from "pokenode-ts";
 import { generationResource } from '$lib/stores/generationStore';
-import type { Writable } from 'svelte/store';
 import { get } from 'svelte/store'
 
 /** @type {import('./$types').PageLoad} */
-export async function load() : Promise<Writable<NamedAPIResource[]>> {
+export async function load() {
   let value = get(generationResource);
   if(value.length && value.length > 0) {
     return generationResource;
@@ -15,7 +14,6 @@ export async function load() : Promise<Writable<NamedAPIResource[]>> {
 
   if (generations) {
       generationResource.set(generations.results);
-      return generationResource;
   }
  
   throw error(404, 'Not found');
